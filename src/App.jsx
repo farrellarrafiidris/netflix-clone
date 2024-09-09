9fc8277d0937dd1e381add4a47259066a3ac9121
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-09-09 11:30:56 +0700

    Revert "adding login page"
    This reverts commit 0938e5cafa04ccc1596ac242a00529efd003045b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import Home from "./pages/Home/Home"
-import {Routes, Route} from 'react-router-dom'
-import Login from "./pages/Login/Login"
 
 const App = () => {
   return (
     <main>
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/> 
-      </Routes>
+      <Home/>
       
     </main>
   )
