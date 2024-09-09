69b39d46ce3b3b975ef0c4dead221e6545fa46b0
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-09-09 11:34:04 +0700

    Adding Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Home from "./pages/Home/Home"
+import {Routes, Route} from 'react-router-dom'
+import Login from "./pages/Login/Login"
 
 const App = () => {
   return (
     <main>
-      <Home/>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/> 
+      </Routes>
       
     </main>
   )
